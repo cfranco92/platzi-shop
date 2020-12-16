720e390b353633f2174b66dea2cada6b61aa1309
Cristian Franco <cfrancobedoya@gmail.com>
2020-12-16 14:57:17 -0500

    Modularizing cart component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
-import { ProductsComponent } from './products/components/products.component';
 import { ContactComponent } from './contact/contact.component';
 import { DemoComponent } from './demo/demo.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { LayoutComponent } from './layout/layout.component';
 import { AdminGuard } from './admin.guard';
+import { CartComponent } from './cart/components/cart.component';
 
 const routes: Routes = [
   {
@@ -36,6 +36,10 @@ const routes: Routes = [
         canActivate: [AdminGuard],
         component: ContactComponent
       },
+      {
+        path: 'cart',
+        loadChildren: () => import('./cart/cart.module').then(m => m.CartModule)
+      }
     ]
   },
   {
