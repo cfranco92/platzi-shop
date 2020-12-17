1664fcc4e4e006abd7f1f33ff5335d54330605fe
Cristian Franco <cfrancobedoya@gmail.com>
2020-12-17 12:39:43 -0500

    Creating header

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,15 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatBadgeModule, MatIconModule } from '@angular/material';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatBadgeModule
   ],
   exports: [
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatBadgeModule
   ]
 })
 export class MaterialModule { }
